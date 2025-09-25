b020cbc07ff97e073546bd742c23f8ef63d724a8
Omsai Alladwar <omsaialladwar@gmail.com>
2025-09-25 18:57:08 +0530

    .

--- Python/Practice/Demo.py
+++ Python/Practice/Demo.py
@@ -1,11 +1,14 @@
-def caller():
-    def show1():
-        print("show 1")
-    return show1
+dict=[['c',10.0], ['b',20.0], ['a',10.0], ['d',0.0]]
+scores = sorted(set([score for name,score in dict]))
+print(scores)
+max2 = scores[1]
+print(max2)
+selected = [[name, score] for name, score in dict if score == max2]
+print(selected)
+names = [name for name,score in selected]
+print(names)
+names.sort()
+print(names)
+for i in names:
+    print(i)
 
-print("1st way")
-fun = caller()
-fun()
-
-print("2nd way")
-caller()()
