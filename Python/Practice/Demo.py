f1ed3ca970b4927da88a6282e9e5e82d18e1017c
Omsai Alladwar <omsaialladwar@gmail.com>
2025-09-27 08:19:18 +0530

    .

--- Python/Practice/Demo.py
+++ Python/Practice/Demo.py
@@ -1,14 +1,1 @@
-dict=[['c',10.0], ['b',20.0], ['a',10.0], ['d',0.0]]
-scores = sorted(set([score for name,score in dict]))
-print(scores)
-max2 = scores[1]
-print(max2)
-selected = [[name, score] for name, score in dict if score == max2]
-print(selected)
-names = [name for name,score in selected]
-print(names)
-names.sort()
-print(names)
-for i in names:
-    print(i)
-
+print((lambda *a: [i for i in a])(1,2,3) )
---
+++ Python/Python_Day_5/Lambda/Examples/Third.py
@@ -0,0 +1,6 @@
+def myfun(fun,num):
+    fun(num)
+
+disp=lambda x: [print(i) for i in range(1,x)]
+
+myfun(disp,7)
