89248b3e168559cf0ace3c35001fe008b016c3c8
Omsai Alladwar <omsaialladwar@gmail.com>
2025-10-03 09:07:10 +0530

    .

--- Python/Practice/demo.py
+++ Python/Practice/demo.py
@@ -1,4 +1,15 @@
-s = "A man, a plan, a canal: Panama"
-a=""
-a += "".join([x for x in s[::] if x.isalpha()])
-print(a)
+# how to write inside a file and read also using "w+" mode ( write and read mode)
+
+
+with open("myfile.txt","w+") as f:
+    print("enter messages, stop to quit\n")
+    while True:
+        str=input()
+        if str=="stop":
+            break
+        f.writelines(str+"\n")
+    f.seek(0, 0)  # place the file pointer at the beginning
+    data=f.read()
+    print(data)
+print("Done")
+
