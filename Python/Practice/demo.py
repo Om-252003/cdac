a02cb5ac555998e79bbc076ee8a77b476c6669b9
Omsai Alladwar <omsaialladwar@gmail.com>
2025-10-06 09:51:26 +0530

    .

--- Python/Practice/demo.py
+++ Python/Practice/demo.py
@@ -1,19 +1,17 @@
-nums = [1,2,3]
-if len(nums) == 1:
-    print(nums[0])
-
-evnsum, oddsum  = 0,0
-if len(nums) > 1:
-
-    i=0
-    while(i<len(nums)):
-        evnsum += nums[i]
-        i+=2
-    j=1
-    while(j<len(nums)):
-        oddsum +=nums[j]
-        j+=2
-
-print(evnsum-oddsum)
-
-
+s = "   -042"
+temp = s.strip()
+print(temp)
+for i in temp:
+    if i == '-':
+        sign = -1
+# sums=0
+# while temp>0 :
+#     r = temp%10
+#     temp=temp//10
+#     sums=(sums*10) + r
+#
+#     if (sums < (-2) ** 31)  or sums  > ((2 ** 31) - 1):
+#         print(0)
+#         exit(123)
+#
+# print(-sums) if x<0 else print(sums)
